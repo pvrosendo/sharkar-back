9812fefd4573de203ebaa2ceea00394f04d170e7
PV <paulovitor.rsd@gmail.com>
2025-02-14 22:46:57 -0300

    add: init repo

---
+++ src/main/java/com/rosendo/register_car/RegisterCarApplication.java
@@ -0,0 +1,13 @@
+package com.rosendo.register_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterCarApplication.class, args);
+	}
+
+}
